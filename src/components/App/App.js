f22fd81769c1dfd5010383d481131b12b38b25c7
Farhad Ghayour <me@farhadg.com>
2016-09-21 14:15:11 -0700

    update: message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => (
   <div>
     <h1>HELLO, WORLD!</h1>
-    <p>Please review docs and the various for the lessons on styling React component.</p>
+    <p>Please review docs and this repo's branches for different ways of styling React components.</p>
   </div>
 );
 
